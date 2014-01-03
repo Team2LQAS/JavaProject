ae539e36b3e4395c0a70fe3c8ee9a98eb5d78262
Team2LQAS <team2lqas@gmail.com>
2014-01-03 17:09:46 +0200

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,74 @@
+public class Students {
+	private String name;
+	private int course;
+	private int age;
+	
+	public Students (String name, int course, int age) {
+		this.name = name;
+		this.course = course;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Students [name=" + name + ", course=" + course + ", age=" + age
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + course;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Students other = (Students) obj;
+		if (age != other.age)
+			return false;
+		if (course != other.course)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCourse() {
+		return course;
+	}
+
+	public void setCourse(int course) {
+		this.course = course;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}
